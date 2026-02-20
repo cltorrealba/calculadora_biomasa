1371a564cd831c8bcc6aedc60bbff505167f1a8b
CTorrealba <cltorrealba@uc.cl>
2026-02-20 08:39:53 -0300

    Actualizar credenciales Firebase correctas

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInAnonymously } from 'firebase/auth'
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBC7qg0f_IG-LYkVLqFzGBzDqJOL0P0c4w",
+  apiKey: "AIzaSyDTF5sVhvpCuv74bquuVz2FfWyRr-46XwM",
   authDomain: "calculadora-yeast.firebaseapp.com",
   projectId: "calculadora-yeast",
-  storageBucket: "calculadora-yeast.appspot.com",
-  messagingSenderId: "1161870708",
-  appId: "1:1161870708:web:7a8f9c5a5b8c7d6e5f4g"
+  storageBucket: "calculadora-yeast.firebasestorage.app",
+  messagingSenderId: "108683121725",
+  appId: "1:108683121725:web:63cea1355bbb8a83858654"
 }
 
 const app = initializeApp(firebaseConfig)
